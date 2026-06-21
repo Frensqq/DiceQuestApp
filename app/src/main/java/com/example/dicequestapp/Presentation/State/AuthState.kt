8c3149fec19077c32ab79c2d4005aff699f6aa3d
Frensqq <liadovn@yandex.ru>
2026-06-21 21:27:42 +0300

    add AuthState for authorization screen

---
+++ app/src/main/java/com/example/dicequestapp/Presentation/State/AuthState.kt
@@ -0,0 +1,28 @@
+package com.example.netlibrary.Presentation.State
+
+import com.example.dq_net_library.Domain.Model.User.User
+
+
+data class AuthState(
+
+    var isLoading:Boolean = false,
+    var isSuccess:Boolean =false,
+    var error: String?=null,
+    var emailError: String? = null,
+    var passwordError: String? = null,
+    var confirmPasswordError: String? = null,
+    var generalError: String? = null,
+    var isNotInternet:Boolean = false,
+
+
+    val username: String = "",
+    val email: String = "",
+    val password: String = "",
+    val passwordConfirm: String = password,
+
+    var user: User? =  null,
+
+    var token: String = "",
+)
+
+
